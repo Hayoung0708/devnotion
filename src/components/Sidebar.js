7f4d9ef068c7f366a67cc5e1df47181b39e2ef4c
Hayoung Kang <rkdgkdudsam@naver.com>
2025-04-10 02:28:50 +0900

    Refactor: setting modal 분리

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,6 @@
 import { getDocuments, createDocument } from "../api.js";
 
 export default function Sidebar({ $app, initialState, handleClickDocument }) {
-  this.state = initialState;
-  this.isFetched = false;
-
-  this.handleClickDocument = handleClickDocument;
-  this.$modal = document.querySelector(".modalBg")
-  this.$target = document.createElement("div");
-  this.$target.className = "sideBar";
-  $app.appendChild(this.$target);
-
   this.template = () => `
     <div class="header">
       <div class="profile">
@@ -20,6 +11,37 @@ export default function Sidebar({ $app, initialState, handleClickDocument }) {
         </div>
       </div>
       <button class="setting"></button>
+    </div>
+
+    <div class="modalBg none">
+      <div class="modal">
+          <div class="user">
+            <div class="userTitle">계정</div>
+            <div class="userContents">
+              <div class="profileImage">
+                  <img src="/images/profile.png">
+                  <button>사진 추가</button>
+              </div>
+              <div class="profileName">
+                  <div>선호하는 이름</div>
+                  <input type="text" id="profileNameInput" value="Devcourse"></input>
+              </div>
+            </div>
+          </div>
+          <div class="theme">
+            <div class="themeTitle">기본설정</div>
+            <div class="themeContents">
+              <div>
+                  <div>테마</div>
+                  <div class="subText">내 기기에서 Notion의 모습을 마음껏 바꿔보세요.</div>
+              </div>
+              <ul class="themeList">
+                  <li>다크모드</li>
+                  <li class="none">라이트모드</li>
+              </ul>
+            </div>
+          </div>
+      </div>
     </div>
 
     <form class="search">
@@ -40,6 +62,14 @@ export default function Sidebar({ $app, initialState, handleClickDocument }) {
       </div>
     </div>
   `;
+
+  this.state = initialState;
+  this.isFetched = false;
+
+  this.handleClickDocument = handleClickDocument;
+  this.$target = document.createElement("div");
+  this.$target.className = "sideBar";
+  $app.appendChild(this.$target);
 
   this.renderDocumentTree = (documents) => {
     return documents
@@ -62,167 +92,170 @@ export default function Sidebar({ $app, initialState, handleClickDocument }) {
       .join("");
   };
 
-  this.renderDocuments = () => {
-    const documentList = this.$target.querySelector(".documents ul");
-    if (!documentList) return;
-    documentList.innerHTML = this.renderDocumentTree(this.state);
-  };
+  document.addEventListener("DOMContentLoaded", () => {
+    this.renderDocuments = () => {
+      const documentList = this.$target.querySelector(".documents ul");
+      if (!documentList) return;
+      documentList.innerHTML = this.renderDocumentTree(this.state);
+    };
 
-  this.fetchDocuments = async () => {
-    if (this.isFetched) return;
-    this.isFetched = true;
+    this.fetchDocuments = async () => {
+      if (this.isFetched) return;
+      this.isFetched = true;
 
-    try {
-      const documents = await getDocuments();
-      this.state = documents;
-      this.renderDocuments();
-    } catch (error) {
-      console.log("문서 목록을 불러오는 데 실패했습니다.", error);
-    }
-  };
-
-
-  // 새페이지 생성
-  this.fetchData = async () => {
-    try {
-      const newDocumentData = await createDocument({
-        title: "파일 제목",
-        parent: null,
-      });
-      this.state = [...this.state, newDocumentData];
-      this.renderDocuments();
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  // 하위문서 생성
-  this.createSubDocument = async (parentDocumentId) => {
-    try {
-      const newDocumentData = await createDocument({
-        title: "하위 문서",
-        parent: parentDocumentId,
-      });
-
-      const parentDocument = this.state.find(
-        (doc) => doc.id === parseInt(parentDocumentId)
-      );
-      if (parentDocument) {
-        parentDocument.documents.push(newDocumentData);
+      try {
+        const documents = await getDocuments();
+        this.state = documents;
         this.renderDocuments();
+      } catch (error) {
+        console.log("문서 목록을 불러오는 데 실패했습니다.", error);
       }
-    } catch (error) {
-      console.error("하위 문서 생성 실패:", error);
-    }
-  };
+    };
 
-  // 검색기능
-  this.filter = () => {
-    const searchInput = this.$target.querySelector(".search input");
-    const documentList = this.$target.querySelector(".documents ul");
+    // 새페이지 생성
+    this.fetchData = async () => {
+      try {
+        const newDocumentData = await createDocument({
+          title: "파일 제목",
+          parent: null,
+        });
+        this.state = [...this.state, newDocumentData];
+        this.renderDocuments();
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-    if (!documentList) return;
+    // 하위문서 생성
+    this.createSubDocument = async (parentDocumentId) => {
+      try {
+        const newDocumentData = await createDocument({
+          title: "하위 문서",
+          parent: parentDocumentId,
+        });
 
-    const searchTerm = searchInput.value.trim().toLowerCase();
-    const links = Array.from(documentList.querySelectorAll("li"));
-
-    links.forEach((link) => {
-      const documentLink = link.querySelector("a");
-      if (!documentLink) return;
-
-      const pageTitle = documentLink.textContent.toLowerCase();
-      link.style.display = pageTitle.includes(searchTerm) ? "" : "none";
-    });
-  };
-
-  // 하위문서 토글
-  this.addEventListeners = () => {
-    const documentList = this.$target.querySelector(".documents ul");
-    const setting = this.$target.querySelector(".setting");
-
-    documentList.addEventListener("click", async (event) => {
-      const parentDocumentId = event.target
-        .closest(".document")
-        .querySelector(".title").id;
-
-      console.log(parentDocumentId);
-
-      if (event.target.classList.contains("title")) {
-        const onList = event.target.parentElement.querySelector(".sub");
-        if (onList) {
-          onList.classList.toggle("none");
+        const parentDocument = this.state.find(
+          (doc) => doc.id === parseInt(parentDocumentId)
+        );
+        if (parentDocument) {
+          parentDocument.documents.push(newDocumentData);
+          this.renderDocuments();
         }
+      } catch (error) {
+        console.error("하위 문서 생성 실패:", error);
+      }
+    };
+
+    // 검색기능
+    this.filter = () => {
+      const searchInput = this.$target.querySelector(".search input");
+      const documentList = this.$target.querySelector(".documents ul");
+
+      if (!documentList) return;
+
+      const searchTerm = searchInput.value.trim().toLowerCase();
+      const links = Array.from(documentList.querySelectorAll("li"));
+
+      links.forEach((link) => {
+        const documentLink = link.querySelector("a");
+        if (!documentLink) return;
+
+        const pageTitle = documentLink.textContent.toLowerCase();
+        link.style.display = pageTitle.includes(searchTerm) ? "" : "none";
+      });
+    };
+
+    // 하위문서 토글
+    this.addEventListeners = () => {
+      const documentList = this.$target.querySelector(".documents ul");
+      const setting = this.$target.querySelector(".setting");
+
+      documentList.addEventListener("click", async (event) => {
+        const parentDocumentId = event.target
+          .closest(".document")
+          .querySelector(".title").id;
+
+        console.log(parentDocumentId);
+
+        if (event.target.classList.contains("title")) {
+          const onList = event.target.parentElement.querySelector(".sub");
+          if (onList) {
+            onList.classList.toggle("none");
+          }
+        }
+
+        await handleClickDocument(parentDocumentId);
+
+        if (event.target.classList.contains("add-icon")) {
+          this.createSubDocument(parentDocumentId);
+        }
+      });
+
+      this.$modal = document.querySelector(".modalBg");
+      const theme = this.$modal.querySelector(".themeList");
+
+      // 모달 클릭
+      setting.addEventListener("click", (event) => {
+        console.log("Modal clicked", event);
+        this.$modal.classList.toggle("none");
+      });
+
+      this.$modal.addEventListener("click", (event) => {
+        if (event.target === this.$modal) {
+          this.$modal.classList.add("none");
+        }
+      });
+
+      theme.addEventListener("click", (event) => {
+        console.log("theme clicked", event);
+        const listItems = this.$modal.querySelectorAll(".themeList li");
+
+        listItems.forEach((item) => {
+          item.classList.toggle("none");
+        });
+      });
+
+      const addPage = this.$target.querySelector(".addPage");
+      const searchInput = this.$target.querySelector(".search input");
+
+      if (addPage) {
+        addPage.addEventListener("click", async () => {
+          await this.fetchData();
+        });
       }
 
-      await handleClickDocument(parentDocumentId);
-
-      if (event.target.classList.contains("add-icon")) {
-        this.createSubDocument(parentDocumentId);
+      if (searchInput) {
+        searchInput.addEventListener("input", this.filter);
       }
-    });
 
-    const theme = this.$modal.querySelector(".themeList");
-    // 모달 클릭
-    setting.addEventListener("click", (event) => {
-      console.log("Modal clicked", event);
-      this.$modal.classList.toggle("none")
-    });
+      const info = this.$target.querySelector(".info");
+      const tooltip = info?.querySelector("div");
 
-    this.$modal.addEventListener("click", (event) => {
-      if (event.target === this.$modal) {
-        this.$modal.classList.add("none"); 
+      if (info && tooltip) {
+        info.addEventListener("mouseover", () => {
+          tooltip.className = "";
+        });
+        info.addEventListener("mouseout", () => {
+          tooltip.className = "none";
+        });
       }
-    });
+    };
 
-    theme.addEventListener("click", (event) => {
-      console.log("theme clicked", event);
-      const listItems = this.$modal.querySelectorAll(".themeList li");
-  
-      listItems.forEach(item => {
-        item.classList.toggle("none"); 
+    this.setState = (newState) => {
+      this.state = newState;
+      this.renderDocuments();
+    };
+
+    this.render = () => {
+      this.$target.innerHTML = this.template();
+      this.fetchDocuments();
+      this.addEventListeners();
+    };
+
+    this.render();
+
+    return {
+      setState: this.setState,
+    };
   });
-    });
-
-    const addPage = this.$target.querySelector(".addPage");
-    const searchInput = this.$target.querySelector(".search input");
-
-    if (addPage) {
-      addPage.addEventListener("click", async () => {
-        await this.fetchData();
-      });
-    }
-
-    if (searchInput) {
-      searchInput.addEventListener("input", this.filter);
-    }
-
-    const info = this.$target.querySelector(".info");
-    const tooltip = info?.querySelector("div");
-
-    if (info && tooltip) {
-      info.addEventListener("mouseover", () => {
-        tooltip.className = "";
-      });
-      info.addEventListener("mouseout", () => {
-        tooltip.className = "none";
-      });
-    }
-  };
-
-  this.setState = (newState) => {
-    this.state = newState;
-    this.renderDocuments();
-  };
-
-  this.render = () => {
-    this.$target.innerHTML = this.template();
-    this.fetchDocuments();
-    this.addEventListeners();
-  };
-
-  this.render();
-
-  return {
-    setState: this.setState,
-  };
 }
